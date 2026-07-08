d2a4d175593b0221f6a58a9de86a5d421dde4ac7
adrian92612 <adrianvillamin0612@gmail.com>
2026-07-08 15:49:02 +0800

    initial files

---
+++ backend/src/main/java/com/transaction/system/TransactionManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.transaction.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionManagementSystemApplication.class, args);
+	}
+
+}
